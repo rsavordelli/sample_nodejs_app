f0c8d8f2d038a07c1587dff55dc587981573fdb0
Rubem de Lima Savordelli <rsavordelli@gmail.com>
2017-08-21 17:06:33 +0000

    changing portwq

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World2!')
 })
 
+app.get('/dev', function (req, res) {
+  res.send('Hello in dev ')
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
